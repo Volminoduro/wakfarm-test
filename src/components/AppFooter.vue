04a6053cec269e94786000e4c1b124b848a4f148
Volminoduro <steve.massicard@gmail.com>
2025-12-02 09:51:20 +0100

    CSS footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
-  <footer class="bg-slate-800 border-t-2 border-amber-500 mt-8">
-    <div class="border-t-4 border-slate-700 mb-3"></div>
-    <div class="px-8 py-4">
+  <footer class="bg-slate-800 border-t-2">
+    <div class="border-t-3 border-slate-700 mb-1"></div>
+    <div class="px-8 py-3">
       <div class="flex justify-between items-center text-xs text-slate-400">
         <p>Wakfarm - Calculateur de rentabilité Wakfu</p>
         <p>Projet non officiel - Wakfu © Ankama Games</p>
